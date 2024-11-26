60b6f6b5fc6a7a74e4d1bf2586f138333c241521
CusmeCarlos <carlos183-96@hotmail.es>
2024-11-25 22:53:50 -0500

    Descripción de los cambios realizados

--- config/db.js
+++ config/db.js
@@ -1,17 +1,20 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
+require('dotenv').config();  // Asegúrate de cargar el archivo .env
 
+// Obtener las variables de entorno
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME,       // Nombre de la base de datos
+  process.env.DB_USER,       // Usuario de la base de datos
+  process.env.DB_PASSWORD,   // Contraseña de la base de datos
   {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false,
+    host: process.env.DB_HOST, // Dirección del servidor
+    port: process.env.PORT, // Puerto
+    dialect: 'mysql',          // Dialecto de la base de datos (mysql en este caso)
+    logging: false,            // Desactivar logs de SQL
   }
 );
 
+// Verificar la conexión a la base de datos
 sequelize.authenticate()
   .then(() => console.log('Conexión a la base de datos exitosa'))
   .catch((err) => console.error('Error al conectar la base de datos:', err));
