f30a7ace32815ec012ca3ac3b599cba3086f8b32
CusmeCarlos <carlos183-96@hotmail.es>
2024-11-26 08:49:55 -0500

    Descripción de los cambios realizados

--- config/db.js
+++ config/db.js
@@ -3,9 +3,22 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+// Extraemos el host y el puerto (en este caso ya conocemos que el puerto es 37100)
+const [host, port] = process.env.DB_HOST.split(':');
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+    host: host,                  // Host sin el puerto
+    port: 37100,                 // Puerto que proporcionaste (37100)
     dialect: 'mysql',
+    dialectOptions: {
+        connectTimeout: 60000 // Aumenta el tiempo de espera si es necesario
+    },
+    pool: {
+        max: 5,                // Número máximo de conexiones en el pool
+        min: 0,                // Número mínimo de conexiones en el pool
+        acquire: 30000,        // Tiempo máximo para obtener una conexión
+        idle: 10000,           // Tiempo máximo para que una conexión permanezca inactiva
+    },
 });
 
 sequelize.authenticate()
